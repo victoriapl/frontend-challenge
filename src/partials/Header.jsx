38e54818b0ace5bc524f6074c322c32ad5313453
VictoriaPlazaPalos <victoriapp@ext.zara.com>
2022-06-07 21:00:09 +0200

    Add language preference in localstorage and change design

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import EN from "./../icons/en_lng.jpg";
 import ES from "./../icons/es_lng.png";
 
 function Header() {
   const { i18n } = useTranslation();
+  const [selectedLanguage, setLanguage] = useState("es");
+
+  const languages = [
+    { key: "en", icon: EN },
+    { key: "es", icon: ES },
+  ];
+
+  useEffect(() => {
+    const language = localStorage.getItem("language");
+    if (language) {
+      setLanguage(language);
+    }
+  }, []);
+
+  const changeLanguage = (language) => {
+    setLanguage(language);
+    i18n.changeLanguage(language);
+    localStorage.setItem("language", language);
+  };
 
   return (
     <header className="sticky top-0 bg-white border-b border-slate-200 z-30">
@@ -15,12 +34,24 @@ function Header() {
 
           {/* Header: Right side */}
           <div className="flex items-center space-x-3">
-            <button onClick={() => i18n.changeLanguage("en")}>
-              <img src={EN} width={40} style={{ height: 25 }} />
-            </button>
-            <button onClick={() => i18n.changeLanguage("es")}>
-              <img src={ES} width={40} style={{ height: 25 }} />
-            </button>
+            {languages.map(({ key, icon }) => (
+              <button
+                key={key}
+                onClick={() => changeLanguage(key)}
+                className={`${
+                  selectedLanguage === key
+                    ? "border-2 border-slate-600 rounded-full"
+                    : ""
+                }`}
+              >
+                <img
+                  src={icon}
+                  width={25}
+                  className="rounded-full"
+                  style={{ height: 25 }}
+                />
+              </button>
+            ))}
           </div>
         </div>
       </div>
